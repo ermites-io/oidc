b75a032827287ebd910177d811572c4bc6b02ec5
eau <eau+code@unix4fun.net>
2019-12-24 14:55:36 +0100

    errors are consts

--- const.go
+++ const.go
@@ -2,6 +2,16 @@
 
 package oidc
 
+type Error string
+
+func (e Error) Error() string { return string(e) }
+
 const (
+	ErrParse        = Error("parse error")
+	ErrUnsupported  = Error("unsupported format")
+	ErrInvalid      = Error("invalid input")
+	ErrInvalidState = Error("invalid state")
+	ErrNetwork      = Error("network error")
+
 	MaxUserDataSize = 1024
 )
